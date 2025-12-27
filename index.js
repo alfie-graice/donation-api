6ddff2c13faf41e288a94b2ea9989f441cb804cc
alfie-graice <msafarjaya@gmail.com>
2025-12-27 11:59:56 +0700

    index.js

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ app.post("/api/webhook/saweria", async (req, res) => {
 
   const donation = {
     id: Date.now().toString() + Math.floor(Math.random() * 1000), // ID unik
-    donor: data.donor || data.name || "Anonymous",
+    donor: data.donator_name || "Anonymous",
     amount: Number(data.amount || data.amount_raw || 0),
     message: data.message || "",
     platform: "saweria",
-    matchedUsername: data.username || data.donor || "Anonymous", // username Roblox
+    matchedUsername: data.donator_name || "Anonymous", // username Roblox
     ts: Date.now()
   };
 
@@ -93,3 +93,4 @@ app.post("/api/register/:secret", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Donation API running on port ${PORT}`);
 });
+
